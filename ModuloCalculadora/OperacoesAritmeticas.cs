e80e3077c5f517e502a21eeb6ecb979b8a359d53
srozisca <srozisca@gmail.com>
2021-04-11 18:45:04 -0300

    estudo de montagem de dll

---
+++ ModuloCalculadora/OperacoesAritmeticas.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloCalculadora
+{
+    //Uma classe estática: não pode ser instanciada, não pode herdar, nem ser herdada, só pode conter membros estáticos.
+    /// <summary>
+    /// Classe com as operações matemáticas básicas de adição, subtração, multiplicação e divisão.
+    /// </summary>
+    public static class OperacoesAritmeticas 
+    {
+        /// <summary>
+        /// Operação básica de adição
+        /// </summary>
+        /// <param name="parcela1">
+        /// A primeira parcela
+        /// </param>
+        /// <param name="parcela2">
+        /// A segunda parcela
+        /// </param>
+        /// <returns>A soma dos números informados</returns>
+        public static double Adicao(double parcela1, double parcela2)
+        {
+            return parcela1 + parcela2;
+        }
+
+        /// <summary>
+        /// Operação básica de subtração
+        /// </summary>
+        /// <param name="minuando">
+        /// O minuendo
+        /// </param>
+        /// <param name="subtraendo">
+        /// O subtraendo
+        /// </param>
+        /// <returns>
+        /// A diferença dos números informados.
+        /// </returns>
+        public static double Subtracao(double minuando, double subtraendo)
+        {
+            return minuando - subtraendo;
+        }
+
+        /// <summary>
+        /// Operação básica de multiplicação
+        /// </summary>
+        /// <param name="multiplicando">
+        /// O multiplicando
+        /// </param>
+        /// <param name="multiplicador">
+        /// O multiplicador
+        /// </param>
+        /// <returns>O produto dos números informados.</returns>
+        public static double Multiplicacao(double multiplicando, double multiplicador)
+        {
+            return multiplicando * multiplicador;
+        }
+
+        /// <summary>
+        /// Operação básica de divisão
+        /// </summary>
+        /// <param name="dividendo">
+        /// O dividendo
+        /// </param>
+        /// <param name="divisor">
+        /// O divisor
+        /// </param>
+        /// <returns>O quociente dos números informados</returns>
+        public static double Divisao(double dividendo, double divisor)
+        {
+            return dividendo / divisor;
+        }
+
+    }
+}
---
+++ ModuloCalculadora/Tabuada.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloCalculadora
+{
+    /// <summary>
+    /// Calcula a tabuada
+    /// </summary>
+    public class Tabuada
+    {
+        /// <summary>
+        /// Número do qual será calculada a tabuada.
+        /// </summary>
+        public int Numero { get; }
+        public Tabuada(int numero)
+        {
+            Numero = numero;
+        }
+
+        /// <summary>
+        /// Realiza o cálculo da tabuada.
+        /// </summary>
+        /// <param name="de">
+        /// Início do intervalo para o cálculo da tabuada
+        /// </param>
+        /// <param name="ate">
+        /// Final do intervalo para o cálculo da tabuada
+        /// </param>
+        /// <returns>
+        /// Tabuada do número informado dentro do intervalo fornecido
+        /// </returns>
+        public string Calcular(int de, int ate)
+        {
+            var sb = new StringBuilder();
+
+            for( int i = de; i<=ate; i++ )
+            {
+                sb.AppendLine($"{Numero} x {i} = {OperacoesAritmeticas.Multiplicacao(Numero,i)}");
+                //10 x 1 = 10
+                //10 x 1 = 20
+            }
+            return sb.ToString();
+        }
+    }
+}
